f7eaa31d71fbe4c77715fe2b843e538580ce2fdf
Seoyoung Cho <bera1125@naver.com>
2023-09-19 22:47:48 +0900

    Week1 과제 제출

---
+++ src/myCalculator.java
@@ -0,0 +1,14 @@
+public class myCalculator implements Calculator{
+    public int plus(int a, int b) {
+        return a+b;
+    }
+    public int minus(int a, int b) {
+        return a-b;
+    }
+    public int mul(int a, int b) {
+        return a*b;
+    }
+    public int div(int a, int b) {
+        return a/b;
+    }
+}
